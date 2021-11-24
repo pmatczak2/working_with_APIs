4dfa49cecb68d077fe90f6f583a074a84882d738
piotrmatczak <pmatczak2@gmail.com>
2021-11-23 16:09:36 -0800

    working with the Response Dictionary

--- python_repos.py
+++ python_repos.py
@@ -1,7 +1,7 @@
 import requests
 
 # make API call and store the resonse.
-url = 'https://api.github.com/search/repositories?q=launguage:python$sort=start'
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
@@ -10,5 +10,12 @@ print(f"Status code: {r.status_code}")
 response_dict = r.json()
 print(f"Total repositories: {response_dict['total_count']}")
 
-# Process results.
-print(response_dict.keys())
+#  Explore information about the repositories.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
