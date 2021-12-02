403e89c8430d9245f094c2ddfc77417ebaa48e05
piotrmatczak <pmatczak2@gmail.com>
2021-12-02 04:40:46 -0800

    Process information about each submission.

--- hn_article.py
+++ hn_article.py
@@ -1,15 +1,13 @@
-import json
-
 import requests
-
+import json
 
 #  Make and API call, and store the response.
 url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
 
-#  Explore the structure of the data.
-response_dic = r.json()
+# Explore the structure of the data.
+response_dict = r.json()
 readable_file = 'data/readable_hn_data.json'
 with open(readable_file, 'w') as f:
-    json.dump(response_dic, f, indent=4)
+    json.dump(response_dict, f, indent=4)
---
+++ hn_submitions.py
@@ -0,0 +1,14 @@
+from operator import itemgetter
+
+import requests
+
+#  Make and API call, and store the response.
+url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+# Process information about each submission.
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:30]:
+    # Make a separate API call for each submission
